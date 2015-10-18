975515dcf9904f53d48ffc1087e8920589c69acf
amirbar <amirb4r@gmail.com>
2015-10-18 19:08:27 +0300

    mock generation

--- src/other/generateMockData/mapLogic.js
+++ src/other/generateMockData/mapLogic.js
@@ -16,22 +16,32 @@
         var placesOutput = [];
         var service = new google.maps.places.PlacesService(map);
         var total_requests = placesList.length;
+        var factor=0;
+        var global_index = 0;
+        for (var i=5; i<placesList.length+5; i+=5){
+            for (var j=i-5; j<i && j<placesList.length; j++) {
+                setTimeout(function () {
+                    var sPlaceName = placesList[global_index];
+                    global_index++;
+                    service.textSearch({query: suffix + sPlaceName}, function (result, status) {
+                        total_requests = total_requests - 1;
+                        console.log(status, "remaining requests: " + total_requests);
+                        if (status == google.maps.places.PlacesServiceStatus.OK) {
 
-        placesList.forEach(function(sPlaceName){
-            service.textSearch({query: suffix + sPlaceName}, function(result, status){
-                total_requests = total_requests-1;
-                if (status == google.maps.places.PlacesServiceStatus.OK){
-
-                    placesOutput.push({
-                        name: result[0]["formatted_address"],
-                        lon: result[0].geometry.location.lng(),
-                        lat: result[0].geometry.location.lat()
+                            placesOutput.push({
+                                name: result[0]["formatted_address"],
+                                lon: result[0].geometry.location.lng(),
+                                lat: result[0].geometry.location.lat()
+                            });
+                            if (total_requests == 0) {
+                                callback(placesOutput);
+                            }
+                        }
                     });
-                    if (total_requests == 0){
-                        callback(placesOutput);
-                    }
-                }
-            });
-        });
+
+                }, factor);
+            }
+            factor += 5000;
+        }
     }
 
